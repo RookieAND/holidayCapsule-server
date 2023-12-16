f72b111027a791177cc8d67c3c06129e147077aa
Baik Gwangin <gwangin1999@naver.com>
2023-12-16 17:22:52 +0900

    feat: 파일 업로드를 위해 multerMiddleware 추가

---
+++ src/middlewares/multer.ts
@@ -0,0 +1,41 @@
+import { Request } from 'express';
+import { NextFunction } from 'express-serve-static-core';
+import multer from 'multer';
+
+type FileNameCallbackFunc = (error: Error | null, filename: string) => void;
+
+/**
+ * 파일 갯수 제한 : 4개, 파일 당 사이즈 제한 : UNDER 10MB
+ */
+const MAX_SIZE = 1024 * 1024 * 10;
+const MAX_AMOUNT = 4;
+
+/**
+ * Express의 Request body 에 존재하는 파일을 읽어 저장하도록 하는 Config
+ * destination 옵션에 정의된 경로에 파일을 저장하며, callback을 통해 파일의 이름으로 저장한다.
+ */
+const multerConfig: multer.Options = {
+  storage: multer.diskStorage({
+    destination: 'tmp_files/',
+    filename: function (
+      _: Request,
+      file: Express.Multer.File,
+      callback: FileNameCallbackFunc,
+    ) {
+      callback(null, file.originalname);
+    },
+  }),
+  limits: {
+    fileSize: MAX_SIZE,
+    files: MAX_AMOUNT,
+  },
+  // 일부 이미지 확장자 (jpeg, png, webp, avif, tiff) 만 허용
+  fileFilter: (_, file, callback) => {
+    callback(
+      null,
+      file.mimetype.match(/(image\/(jpeg|png|webp|avif|tiff))/)?.length !== 0,
+    );
+  },
+};
+
+export const multerMiddleware = multer(multerConfig);
