6dab93cc4e9adb62f31e9cb6481ba1ad9e7c9b51
Baik Gwangin <gwangin1999@naver.com>
2023-12-18 00:51:12 +0900

    chore: prettier 코드 단에 적용

--- src/libs/mongoose/base-method.ts
+++ src/libs/mongoose/base-method.ts
@@ -1,45 +1,53 @@
 import type {
-  FilterQuery,
-  ProjectionType,
-  QueryOptions,
-  Schema,
+    Document,
+    FilterQuery,
+    Model,
+    ProjectionType,
+    QueryOptions,
+    Schema,
 } from 'mongoose';
+
+export type BaseMethodModel<T extends Document> = Model<T> & {
+  findOne(): Promise<T | null>
+  find(): Promise<T[] | null>
+  findOneAndUpdate(): Promise<T>
+}
 
 /**
  * Schema 에 기본적인 Method 을 추가하여 적용하는 mongooseBaseMethodPlugin
  * @param schema Plugin 을 적용할 Schema
  */
 export const mongooseBaseMethodPlugin = (schema: Schema) => {
-  schema.add({
-    id: {
-      type: String,
-      unique: true,
-    },
-  });
+    schema.add({
+        id: {
+            type: String,
+            unique: true,
+        },
+    });
 
-  type SchemaType = typeof schema;
+    type SchemaType = typeof schema;
 
-  schema.methods.findOne = async function (
-    query: FilterQuery<SchemaType>,
-    projection?: ProjectionType<SchemaType>,
-    option?: QueryOptions<SchemaType>,
-  ) {
-    this.findOne(query, projection, option).exec().lean();
-  };
+    schema.methods.findOne = async function (
+        query: FilterQuery<SchemaType>,
+        projection?: ProjectionType<SchemaType>,
+        option?: QueryOptions<SchemaType>,
+    ) {
+        this.findOne(query, projection, option).exec().lean();
+    };
 
-  schema.methods.find = async function (
-    query: FilterQuery<SchemaType>,
-    projection?: ProjectionType<SchemaType>,
-    option?: QueryOptions<SchemaType>,
-  ) {
-    this.find(query, projection, option).exec().lean();
-  };
+    schema.methods.find = async function (
+        query: FilterQuery<SchemaType>,
+        projection?: ProjectionType<SchemaType>,
+        option?: QueryOptions<SchemaType>,
+    ) {
+        this.find(query, projection, option).exec().lean();
+    };
 
-  schema.methods.findOneAndUpdate = async function (
-    query: FilterQuery<SchemaType>,
-    projection?: ProjectionType<SchemaType>,
-    option?: QueryOptions<SchemaType>,
-  ) {
-    this.find(query, projection, option).exec().lean();
-  };
+    schema.methods.findOneAndUpdate = async function (
+        query: FilterQuery<SchemaType>,
+        projection?: ProjectionType<SchemaType>,
+        option?: QueryOptions<SchemaType>,
+    ) {
+        this.findOneAndUpdate(query, projection, option).exec().lean();
+    };
 };
--- src/models/user/model.ts
+++ src/models/user/model.ts
@@ -1,9 +1,14 @@
-import { model } from 'mongoose';
+import { model, type Document } from 'mongoose';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
+import { type BaseMethodModel } from '#/libs/mongoose/base-method';
 import { type UserType, userSchema } from './schema';
 
-export const userModel = model<UserType, SoftDeleteModel<UserType>>(
+type PluginModel<T extends Document> = BaseMethodModel<T> & SoftDeleteModel<T>
+
+type findOne = PluginModel<UserType>['findOne'];
+
+export const userModel = model<UserType, PluginModel<UserType>>(
     'meeting',
     userSchema,
 );
