806db311d277394c7cd9cfaa2d7ac7533ab6c3dc
Baik Gwangin <gwangin1999@naver.com>
2023-12-23 16:02:39 +0900

    feat: 앨범 멤버 Model 및 Schema 설계

---
+++ src/models/album-member/schema.ts
@@ -0,0 +1,49 @@
+import type { Document, Model } from 'mongoose';
+import { Schema } from 'mongoose';
+
+import { mongooseUniqueIdPlugin } from '#/libs/mongoose/unique-id';
+
+export interface AlbumMemberType extends Document {
+    /**
+     * 앨범 멤버 Id
+     */
+    id: string;
+    /**
+     * 소속된 Album Id
+     */
+    albumId: string;
+    /**
+     * 멤버로 참여한 유저 Id
+     */
+    userId: string;
+    /**
+     * 유저의 권한
+     */
+    role: string;
+}
+
+export const albumMemberSchema = new Schema<
+    AlbumMemberType,
+    Model<AlbumMemberType>
+>({
+    id: {
+        type: String,
+        unique: true,
+    },
+    albumId: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true, 
+    },
+    role: {
+        type: String,
+        enum: ['OWNER', 'MEMBER'],
+        required: true,
+    },
+});
+
+albumMemberSchema.index({ albumId: 1, userId: 1 });
+albumMemberSchema.plugin(mongooseUniqueIdPlugin, 'album_member');
